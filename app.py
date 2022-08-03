d3dd974b6b1d1ea50bd746bcd32465f6a5d06270
Le Pham <phamtle724@gmail.com>
2022-08-03 12:29:34 -0700

    created user model, made a base template and new_user_form html

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Blogly application."""
 
 from flask import Flask
-from models import db, connect_db
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -9,4 +10,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+
+
 db.create_all()
+
+# SKELETON
+
--- models.py
+++ models.py
@@ -1,1 +1,48 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+
+
+
+class User(db.Model):
+    """Pet."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(50),
+                     nullable=False)
+    last_name = db.Column(db.String(50),
+                     nullable=False)
+    image_url = db.Column(db.Text,
+                    nullable=True) #maybe DEFAULT here??
+
+
+    # def greet(self):
+    #     """Greet using name."""
+
+    #     return f"I'm {self.name} the {self.species or 'thing'}"
+
+    # def feed(self, units=10):
+    #     """Nom nom nom."""
+
+    #     self.hunger -= units
+    #     self.hunger = max(self.hunger, 0)
+
+    # @classmethod
+    # def get_by_species(cls, species):
+    #     """Get all pets matching that species."""
+
+    #     return cls.query.filter_by(species=species).all()
