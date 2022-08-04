115c857f0c8d0fb97012591129a7d8a2abdadc9c
Edmond Soun <edmond.soun@gmail.com>
2022-08-04 14:19:54 -0700

    added routes for part 2

--- app.py
+++ app.py
@@ -64,8 +64,12 @@ def show_user(user_id):
     last_name = User.query.get(user_id).last_name
     image_url = User.query.get(user_id).image_url
     #user_data = User.q...
+    
+    all_posts = Post.query.all()
+    
     return render_template('user_detail.html', user_id = user_id,
-    first_name = first_name, last_name = last_name, image_url = image_url)
+    first_name = first_name, last_name = last_name, image_url = image_url, 
+    all_posts = all_posts)
 
 @app.get('/users/<int:user_id>/edit')
 def show_edit_user_page(user_id):
@@ -79,7 +83,8 @@ def edit_user(user_id):
     """Process the edit form, returning the user to the /users page"""
 
     form_data = request.form
-
+    
+    #restructure using user_data
     User.query.get(user_id).first_name = form_data["first_name"]
     User.query.get(user_id).last_name = form_data["last_name"]
     User.query.get(user_id).image_url = form_data["image_url"]
@@ -98,3 +103,45 @@ def delete_user(user_id):
     db.session.commit()
     return redirect('/users')
 
+#######
+
+#PART 2 ROUTES
+
+@app.get('/users/<int:user_id>/posts/new')
+def show_new_post_form(user_id):
+    
+    user_data = User.query.get_or_404(user_id)
+
+    return render_template('new_post_form.html', user_data = user_data)
+    
+@app.post('/users/<int:user_id>/posts/new')
+def create_new_post(user_id):
+    
+    form_data = request.form
+    
+    #instantiate new post
+    new_post = Post(title = form_data["title"], content = form_data["content"], user_id = user_id)
+
+    #send post information to database and update
+    db.session.add(new_post)
+    db.session.commit()
+    
+    return redirect(f'/users/{user_id}')
+
+@app.get('/posts/<int:post_id>')
+def show_a_post(post_id):
+    
+    post_data = Post.query.get(post_id)
+    
+    return render_template('show_post.html', post_id = post_id, 
+                            post_data = post_data)
+
+# @app.get('/posts/<int:post_id>/edit')
+# def show_post_edit_form():
+    
+# @app.post('/posts/<int:post_id>/edit')
+# def edit_post():
+    
+# @app.post('/posts/<int:post_id>/delete')
+# def delete_post():
+    
